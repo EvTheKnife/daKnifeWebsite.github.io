39df1bca9cdce1dcbbfd9d6c726d2f019892cc9d
daKnife <evan.jorgensen@gmail.com>
2024-09-12 15:18:56 -0400

    stuff

---
+++ Pages/About_Me.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+
+
+
+    animOrder(document.getElementById("piece1"), 'piece1FirstAnim');
+    animOrder(document.getElementById("piece2"), 'piece2FirstAnim');
+    animOrder(document.getElementById("piece3"), 'piece3FirstAnim');
+    animOrder(document.getElementById("piece4"), 'piece4FirstAnim');
+    animOrder(document.getElementById("piece5"), 'piece5FirstAnim');
+    animOrder(document.getElementById("piece6"), 'piece6FirstAnim');
+    animOrder(document.getElementById("piece7"), 'piece7FirstAnim');
+    animOrder(document.getElementById("piece8"), 'piece8FirstAnim');
+
+
+    function animOrder(piece, firstAnimClass) {
+
+            piece.addEventListener('animationend', function() {
+                piece.classList.add('animFree')
+                piece.classList.remove(firstAnimClass)
+
+            });
+            
+
+    }
+
+}
