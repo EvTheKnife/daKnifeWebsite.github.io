a62bd2a222f4c94be1a79782cf4a715a2b34c887
DaKnife997 <ev.knife@gmail.com>
2025-07-12 15:09:11 -0400

    why am i still doing this

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+window.onload = function() {
+    let gradient = document.querySelector(".mouse-cursor-gradient-tracking");
+    gradient.addEventListener("mousemove", (e) => {
+        let rect = e.target.getBoundingClientRect();
+        let x = e.pageX; // - rect.left;
+        let y = e.pageY; // - rect.top;
+        gradient.style.setProperty("--x", x + "px");
+        gradient.style.setProperty("--y", y + "px");
+    });
+};
+
+
+
 // Open the Modal
 function openModal() {
   document.getElementById("myModal").style.display = "block";
@@ -37,13 +50,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
-window.onload {
-let gradient = document.querySelector(".mouse-cursor-gradient-tracking");
-    gradient.addEventListener("mousemove", (e) => {
-    let rect = e.target.getBoundingClientRect();
-    let x = e.pageX// - rect.left;
-    let y = e.pageY// - rect.top;
-    gradient.style.setProperty("--x", x + "px");
-    gradient.style.setProperty("--y", y + "px");
 }
